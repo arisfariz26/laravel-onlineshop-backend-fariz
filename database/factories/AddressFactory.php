db29cdf1b3c4023c2cf322fa49781c1bdbdbf509
arisfariz26 <arisfariz26@gmail.com>
2024-01-28 16:38:50 +0700

    Address Model, Migration, Factories, and Seeders

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'full_address' => fake()->address(),
+            'phone' => fake()->phoneNumber(),
+            'prov_id' => fake()->numberBetween(1, 34),
+            'city_id' => fake()->numberBetween(1, 100),
+            'district_id' => fake()->numberBetween(1, 100),
+            'postal_code' => fake()->postcode,
+            'user_id' => fake()->numberBetween(1, 10),
+            'is_default' => fake()->boolean,
+        ];
+    }
+}
