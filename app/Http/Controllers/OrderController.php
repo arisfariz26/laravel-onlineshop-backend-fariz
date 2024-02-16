974b6656ed3e1ae5770c1916ab50c9ff636f2d5a
arisfariz26 <arisfariz26@gmail.com>
2024-02-16 20:35:45 +0700

    Laravel Firebase Push Notificatoin

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class OrderController extends Controller
 {
@@ -40,10 +43,32 @@ class OrderController extends Controller
             'shipping_resi' => $request->shipping_resi,
         ]);
 
+         //send notification to user
+         if ($request->status == 'on_delivery') {
+            $this->sendNotificationToUser($order->first()->user_id, 'Paket Dikirim dengan nomor resi ' . $request->shipping_resi);
+        }
+
         //redirect to index
         return redirect()->route('order.index')->with([
             'message' => 'Data Berhasil Diedit!',
         'alert-type' => 'success'
     ]);
+    }
+
+    public function sendNotificationToUser($userId, $message)
+    {
+        // Dapatkan FCM token user dari tabel 'users'
+
+        $user = User::find($userId);
+        $token = $user->fcm_id;
+
+        // Kirim notifikasi ke perangkat Android
+        $messaging = app('firebase.messaging');
+        $notification = Notification::create('Paket Dikirim', $message);
+
+        $message = CloudMessage::withTarget('token', $token)
+            ->withNotification($notification);
+
+        $messaging->send($message);
     }
 }
--- resources/views/pages/user/create.blade.php
+++ resources/views/pages/user/create.blade.php
@@ -110,8 +110,6 @@
                       </div>
 
 
-
-
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                         <div class="col-sm-12 col-md-7">
@@ -134,9 +132,4 @@
 <script src="{{ asset('library/summernote/dist/summernote-bs4.js') }}"></script>
 <script src="{{ asset('library/select2/dist/js/select2.full.min.js') }}"></script>
 <script src="{{ asset('library/upload-preview/jquery.uploadPreview.min.js') }}"></script>
-
-<!-- Page Specific JS File -->
-<script src="{{ asset('js/page/features-post-create.js') }}"></script>
-
-
 @endpush
