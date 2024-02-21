d04089ba71f48b1e7746ff3ae01273a0b6d9277a
arisfariz26 <arisfariz26@gmail.com>
2024-02-21 19:38:47 +0700

    Firebase Push Notificatoin

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -35,4 +35,8 @@ class Order extends Model
         return $this->hasMany(OrderItem::class);
     }
 
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
 }
